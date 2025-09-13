95acca8a399a028bb7fa9d4d64cd0360c88a6ba6
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-09-13 19:55:49 -0400

    Math base contract

---
+++ src/math/MathContract.ts
@@ -0,0 +1,123 @@
+import { u256 } from '@btc-vision/as-bignum/assembly';
+import {
+    Address,
+    AddressMap,
+    Blockchain,
+    BytesWriter,
+    Calldata,
+    ON_OP20_RECEIVED_SELECTOR,
+    OP20,
+    OP20InitParameters,
+    SafeMath,
+    SELECTOR_BYTE_LENGTH,
+} from '@btc-vision/btc-runtime/runtime';
+
+@final
+export class MathContract extends OP20 {
+    public constructor() {
+        super();
+
+        // IMPORTANT. THIS WILL RUN EVERYTIME THE CONTRACT IS INTERACTED WITH. FOR SPECIFIC INITIALIZATION, USE "onDeployment" METHOD.
+    }
+
+    // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
+    public override onDeployment(_calldata: Calldata): void {
+        const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
+        const decimals: u8 = 18; // Your decimals.
+        const name: string = 'Test'; // Your token name.
+        const symbol: string = 'TEST'; // Your token symbol.
+
+        this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
+
+        // Add your logic here. Eg, minting the initial supply:
+        // this._mint(Blockchain.tx.origin, maxSupply);
+    }
+
+    @method(
+        {
+            name: 'address',
+            type: ABIDataTypes.ADDRESS,
+        },
+        {
+            name: 'amount',
+            type: ABIDataTypes.UINT256,
+        },
+    )
+    @emit('Minted')
+    public mint(calldata: Calldata): BytesWriter {
+        this.onlyDeployer(Blockchain.tx.sender);
+
+        this._mint(calldata.readAddress(), calldata.readU256());
+
+        return new BytesWriter(0);
+    }
+
+    /**
+     * Mints tokens to the specified addresses.
+     *
+     * @param calldata Calldata containing an `AddressMap<Address, u256>` to mint to.
+     */
+    @method({
+        name: 'addressAndAmount',
+        type: ABIDataTypes.ADDRESS_UINT256_TUPLE,
+    })
+    @emit('Minted')
+    public airdrop(calldata: Calldata): BytesWriter {
+        this.onlyDeployer(Blockchain.tx.sender);
+
+        const addressAndAmount: AddressMap<u256> = calldata.readAddressMapU256();
+        const addresses: Address[] = addressAndAmount.keys();
+
+        let totalAirdropped: u256 = u256.Zero;
+
+        for (let i: i32 = 0; i < addresses.length; i++) {
+            const address = addresses[i];
+            const amount = addressAndAmount.get(address);
+
+            const currentBalance: u256 = this.balanceOfMap.get(address);
+
+            if (currentBalance) {
+                this.balanceOfMap.set(address, SafeMath.add(currentBalance, amount));
+            } else {
+                this.balanceOfMap.set(address, amount);
+            }
+
+            totalAirdropped = SafeMath.add(totalAirdropped, amount);
+
+            this.createMintedEvent(address, amount);
+        }
+
+        this._totalSupply.set(SafeMath.add(this._totalSupply.value, totalAirdropped));
+
+        return new BytesWriter(0);
+    }
+
+    @method(
+        {
+            name: 'operator',
+            type: ABIDataTypes.ADDRESS,
+        },
+        {
+            name: 'from',
+            type: ABIDataTypes.ADDRESS,
+        },
+        {
+            name: 'amount',
+            type: ABIDataTypes.UINT256,
+        },
+        {
+            name: 'data',
+            type: ABIDataTypes.BYTES,
+        },
+    )
+    @returns({
+        name: 'selector',
+        type: ABIDataTypes.BYTES4,
+    })
+    public onOP20Received(_calldata: Calldata): BytesWriter {
+        const response = new BytesWriter(SELECTOR_BYTE_LENGTH);
+        response.writeSelector(ON_OP20_RECEIVED_SELECTOR);
+
+        return response;
+    }
+}
